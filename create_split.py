8244bc6ca841b54f75e82e79a7a30fab246362db
Brandon Quach <bquach@caltech.edu>
2020-05-10 20:33:14 -1000

    Fixed Location Issue of Split

--- create_split.py
+++ create_split.py
@@ -5,36 +5,43 @@ import json
 from sklearn.model_selection import train_test_split
 
 def create_split(annotations, images):
-    locs = np.random.permutation([0 for i in range(552)])
+    locs = []
+    for item in images:
+        if item['location'] not in locs:
+            locs.append(item['location'])
+    locs = np.random.permutation(locs)
 
     # Split locations into train/val/test 70/20/10
-    train_locs = locs[:round(0.7*552)]
-    val_locs = locs[round(0.7*552):round(0.9*552)]
-    test_locs = locs[round(0.9*552):]
+    n = len(locs)
+    train_locs = locs[:round(0.7*n)]
+    val_locs = locs[round(0.7*n):round(0.9*n)]
+    test_locs = locs[round(0.9*n):]
 
     # Get list of images for each set first and then we'll split annotations
-    train_images = []
-    val_images = []
-    test_images = []
+    train_images = set([])
+    val_images = set([])
+    test_images = set([])
 
     for item in images:
         if item['location'] in train_locs:
             # odd days get assigned to test set
             if int(item['datetime'][8:10]) % 2 == 0:
-                train_images.append(item['id'])
+                train_images.add(item['id'])
             else:
-                test_images.append(item['id'])
+                test_images.add(item['id'])
         elif item['location'] in val_locs:
-            val_images.append(item['id'])
+            val_images.add(item['id'])
         else:
-            test_images.append(item['id'])
-
+            test_images.add(item['id'])
+    print('Images Split Done!')
     train_annotations = []
     y_train = []
     val_annotations = []
     y_val = []
     test_annotations = []
     y_test = []
+
+    print('Splitting Annotations..')
 
     for item in annotations:
         if item['image_id'] in train_images:
@@ -47,11 +54,15 @@ def create_split(annotations, images):
             test_annotations.append(item['id'])
             y_test.append(item['category_id'])
 
+    print(len(train_annotations))
+    print(len(test_annotations))
+    print(len(val_annotations))
+
     # Move 5% to validation
     X_train, X_val, y_train, label_val = train_test_split(train_annotations, y_train, test_size = 0.05)
     val_annotations.extend(X_val)
     y_val.extend(label_val)
-    return train_annotations, val_annotations, test_annotations, y_train, y_val, y_test
+    return X_train, val_annotations, test_annotations, y_train, y_val, y_test
 
 if __name__ == '__main__':
     with open('iwildcam2020_train_annotations.json') as f:
