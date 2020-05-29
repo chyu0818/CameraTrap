eca9102b270012a7b5550faf6dd5a1d69104b3ab
bquach009 <quachb@gpu-0008.hpc.ci.its.hawaii.edu>
2020-05-29 20:49:36 +0000

    Debug Classifier

--- triplet_classifier.py
+++ triplet_classifier.py
@@ -4,6 +4,7 @@ import torchvision.models as models
 from torchvision import transforms as T
 from torch.optim import lr_scheduler
 import torch.optim as optim
+from torch.optim.lr_scheduler import StepLR
 from torch.autograd import Variable
 import json
 import torch.nn.functional as F
@@ -12,8 +13,8 @@ from trainer import fit
 import numpy as np
 import matplotlib
 import matplotlib.pyplot as plt
-from data_reader import CameraTrapCropDataset
-from triplet_loss import Classifier
+from data_reader import CameraTrapCropDataset, CameraTrapEmbeddingDataset
+from triplet_loss import Embedder, Classifier
 
 def train(model, device, train_loader, optimizer, epoch):
     '''
@@ -24,11 +25,12 @@ def train(model, device, train_loader, optimizer, epoch):
     model.train()# Set the model to training mode
     losses = []
     for batch_idx, data0 in enumerate(train_loader):
-        data = data0['image']
+        data = data0['embedding']
         target = data0['target']
         data, target = data.to(device), target.to(device)
+        target = target.long()
         optimizer.zero_grad()               # Clear the gradient
-        outputs = model(data)                # Make predictions
+        outputs = model(data.float())                # Make predictions
         loss = criterion(outputs, target)   # Compute loss
         loss.backward()                     # Gradient computation
         optimizer.step()                    # Perform a single optimization step
@@ -39,19 +41,45 @@ def train(model, device, train_loader, optimizer, epoch):
                 100. * batch_idx / len(train_dataset), losses[-1]))
     return np.mean(losses)
 
-
-def get_preds(model, device, test_loader):
+def test(model, device, test_loader):
+    criterion = nn.CrossEntropyLoss(reduction='sum')
     model.eval()    # Set the model to inference mode
-    predictions = []
-    labels = []
+    test_loss = 0
+    correct = 0
+    total = 0
+    with torch.no_grad():   # For the inference step, gradient is not computed
+        for data0 in test_loader:
+            data = data0['embedding']
+            target = data0['target']
+            data, target = data.to(device), target.to(device)
+            target = target.long()
+            output = model(data.float())
+            test_loss += criterion(output, target).item()  # sum up batch loss
+            pred = output.argmax(dim=1, keepdim=True)  # get the index of the max log-probability
+            correct += pred.eq(target.view_as(pred)).sum().item()
+            total += len(target)
+
+    test_loss /= total
+
+    print('\nTest set: Average loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n'.format(
+        test_loss, correct, total,
+        100. * correct / total))
+    return test_loss
+
+def get_embeddings(model, device, test_loader):
+    model.eval()    # Set the model to inference mode
+    embeddings = np.zeros((len(test_loader.dataset), 1000))
+    labels = np.zeros(len(test_loader.dataset))
+    k = 0
     with torch.no_grad():   # For the inference step, gradient is not computed
         for data0 in test_loader:
             data = data0['image']
             target = data0['target']
             data, target = data.to(device), target.to(device)
-            output = model(data)
-            predictions.extend(output)
-            labels.extend(target)
+            embeddings[k:k + len(data)] = model(data).cpu().numpy()
+            labels[k: k + len(data)] = target.cpu().numpy()
+            k += len(data)
+    return embeddings, labels
 
 
 cuda = torch.cuda.is_available()
@@ -93,7 +121,6 @@ print('\nVal Trans-Location Data')
 val_trans_dataset = CameraTrapCropDataset(img_path, val_trans_path, ann_path, bbox_path,
                                   percent_data, transform=transform_val)
 
-
 train_loader = torch.utils.data.DataLoader(
         train_dataset, batch_size=BATCH_SIZE_TRAIN, shuffle=True, **kwargs
 )
@@ -109,14 +136,83 @@ embedding_net = models.resnet18(pretrained=True)
 #     param.requires_grad = False
 embedding_net.fc = torch.nn.Linear(512, 1000)
 
-model = Classifier(embedding_net)
+model = Embedder(embedding_net)
 model.load_state_dict(torch.load(weights_path))
 
 for param in model.parameters():
     param.requires_grad = False
 
-
 device = "cuda"
 model.to(device)
+
+print("Obtaining Embeddings!")
+
+X_train, y_train = get_embeddings(model, device, train_loader)
+print("Got Train Embeddings!", X_train.shape, y_train.shape)
+
+X_val_cis, y_val_cis = get_embeddings(model, device, val_cis_loader)
+print("Got Val Cis Embeddings")
+
+X_val_trans, y_val_trans = get_embeddings(model, device, val_trans_loader)
+print("Got Val Trans Embeddings")
+
+train_dataset = CameraTrapEmbeddingDataset(X_train, y_train)
+val_cis_dataset = CameraTrapEmbeddingDataset(X_val_cis, y_val_cis)
+val_trans_dataset = CameraTrapEmbeddingDataset(X_val_trans, y_val_trans)
+
+train_loader = torch.utils.data.DataLoader(
+        train_dataset, batch_size=BATCH_SIZE_TRAIN, shuffle=True, **kwargs
+)
+val_cis_loader = torch.utils.data.DataLoader(
+        val_cis_dataset, batch_size=BATCH_SIZE_VAL, shuffle=True, **kwargs
+)
+val_trans_loader = torch.utils.data.DataLoader(
+        val_trans_dataset, batch_size=BATCH_SIZE_VAL, shuffle=True, **kwargs
+)
+
+model = Classifier()
+model.to(device)
+
+
+print("Starting Training!")
+
+lr = 1
+optimizer = optim.Adadelta(model.parameters(), lr=lr)
+
+step = 1
+gamma = 0.7
+scheduler = StepLR(optimizer, step_size=step, gamma=gamma)
+
+# Training loop
+train_losses = []
+test_cis_losses = []
+test_trans_losses = []
+start = time.time()
+for epoch in range(1, NUM_EPOCHS + 1):
+    train_loss = train(model, device, train_loader, optimizer, epoch)
+    test_cis_loss = test(model, device, val_cis_loader)
+    test_trans_loss = test(model, device, val_trans_loader)
+    train_losses.append(train_loss)
+    test_cis_losses.append(test_cis_loss)
+    test_trans_losses.append(test_trans_loss)
+    scheduler.step()    # learning rate scheduler
+    torch.save(model.state_dict(), "triplet_classifier_{}_{}.pt".format(percent_data,epoch))
+print('Train Time:', time.time()-start)
+# You may optionally save your model at each epoch here
+np.save("train_triplet_classifier_loss{}.npy".format(percent_data), np.array(train_losses))
+np.save("test_triplet_classifier_cis_loss{}.npy".format(percent_data), np.array(test_cis_losses))
+np.save("test_triplet_classifier_trans_loss{}.npy".format(percent_data), np.array(test_trans_losses))
+
+print("\nFinal Performance!")
+print("Validation Set (cis):")
+test(model, device, val_cis_loader)
+print("Validation Set (trans):")
+test(model, device, val_trans_loader)
+print("Training Set:")
+test(model, device, train_loader)
+
+
+
+
 
 
