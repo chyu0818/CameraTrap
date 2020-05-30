7a59f06ee20666cad039649f0f993efd6385f8e1
Christine Yu <christineyu@Christines-MacBook-Pro.local>
2020-05-30 01:51:14 -0700

    why were there print statements

--- trainer.py
+++ trainer.py
@@ -65,10 +65,7 @@ def train_epoch(train_loader, model, loss_fn, optimizer, cuda, log_interval, met
     losses = []
     total_loss = 0
 
-    print('train')
-    print(train_loader)
     for batch_idx, (data, target) in enumerate(train_loader):
-        print(batch_idx)
         target = target if len(target) > 0 else None
         if not type(data) in (tuple, list):
             data = (data,)
